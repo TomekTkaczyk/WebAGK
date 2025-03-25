c496aa0f457a1010fa4381e41f1869baaf885856
Tomek Tkaczyk <devcode@unipromax.pl>
2025-03-25 23:27:28 +0100

    20250325

---
+++ src/modules/Agents/WebAGK.Module.Agents.Core/DAL/Migrations/20250324071816_Init.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAGK.Module.Agents.Core.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Agents");
+
+            migrationBuilder.CreateTable(
+                name: "Agents",
+                schema: "Agents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: true),
+                    FirstName = table.Column<string>(type: "text", nullable: true),
+                    SecondName = table.Column<string>(type: "text", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    PersonalId = table.Column<string>(type: "text", nullable: true),
+                    TaxId = table.Column<string>(type: "text", nullable: true),
+                    RpuId = table.Column<string>(type: "text", nullable: true),
+                    Address = table.Column<string>(type: "text", nullable: true),
+                    IsCompany = table.Column<bool>(type: "boolean", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModifiedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ConcurrencyStamp = table.Column<Guid>(type: "uuid", nullable: false),
+                    xmin = table.Column<uint>(type: "xid", rowVersion: true, nullable: false),
+                    ActiveStatus = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agents", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agents_PersonalId",
+                schema: "Agents",
+                table: "Agents",
+                column: "PersonalId",
+                unique: true,
+                filter: "\"PersonalId\" IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agents_TaxId",
+                schema: "Agents",
+                table: "Agents",
+                column: "TaxId",
+                unique: true,
+                filter: "\"TaxId\" IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Agents",
+                schema: "Agents");
+        }
+    }
+}
---
+++ src/modules/Insurers/WebAGK.Module.Insurers.Core/DAL/Migrations/20250325061216_Init.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAGK.Module.Insurers.Core.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Insurers");
+
+            migrationBuilder.CreateTable(
+                name: "Agents",
+                schema: "Insurers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModifiedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ConcurrencyStamp = table.Column<Guid>(type: "uuid", nullable: false),
+                    xmin = table.Column<uint>(type: "xid", rowVersion: true, nullable: false),
+                    ActiveStatus = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Insurers",
+                schema: "Insurers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModifiedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ConcurrencyStamp = table.Column<Guid>(type: "uuid", nullable: false),
+                    xmin = table.Column<uint>(type: "xid", rowVersion: true, nullable: false),
+                    ActiveStatus = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Insurers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Structures",
+                schema: "Insurers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModifiedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ConcurrencyStamp = table.Column<Guid>(type: "uuid", nullable: false),
+                    xmin = table.Column<uint>(type: "xid", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Structures", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Structures_Insurers_Id",
+                        column: x => x.Id,
+                        principalSchema: "Insurers",
+                        principalTable: "Insurers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Nodes",
+                schema: "Insurers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    StructureId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ParentId = table.Column<Guid>(type: "uuid", nullable: true),
+                    AgentId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Left = table.Column<int>(type: "integer", nullable: false),
+                    Right = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModifiedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    ConcurrencyStamp = table.Column<Guid>(type: "uuid", nullable: false),
+                    xmin = table.Column<uint>(type: "xid", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nodes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Nodes_Agents_AgentId",
+                        column: x => x.AgentId,
+                        principalSchema: "Insurers",
+                        principalTable: "Agents",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Nodes_Nodes_ParentId",
+                        column: x => x.ParentId,
+                        principalSchema: "Insurers",
+                        principalTable: "Nodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Nodes_Structures_StructureId",
+                        column: x => x.StructureId,
+                        principalSchema: "Insurers",
+                        principalTable: "Structures",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Nodes_AgentId",
+                schema: "Insurers",
+                table: "Nodes",
+                column: "AgentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Nodes_ParentId",
+                schema: "Insurers",
+                table: "Nodes",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Nodes_StructureId",
+                schema: "Insurers",
+                table: "Nodes",
+                column: "StructureId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Nodes",
+                schema: "Insurers");
+
+            migrationBuilder.DropTable(
+                name: "Agents",
+                schema: "Insurers");
+
+            migrationBuilder.DropTable(
+                name: "Structures",
+                schema: "Insurers");
+
+            migrationBuilder.DropTable(
+                name: "Insurers",
+                schema: "Insurers");
+        }
+    }
+}
